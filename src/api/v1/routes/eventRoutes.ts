c9ceb15dd6e3f974af5abaa83a0625bafd44c96c
Leann Mekler <lmekler@rrc.ca>
2026-02-12 13:20:30 -0600

    Install morgan for http request logging, implement event creation validation in route handler

--- src/api/v1/routes/eventRoutes.ts
+++ src/api/v1/routes/eventRoutes.ts
@@ -1,11 +1,15 @@
 import express, { Router } from "express";
 import { createEvent } from "../controllers/eventController"
+import { validateRequest } from "../middleware/validate";
+import { postSchemas } from "../validation/postSchemas";
+
 
 // getAllEvents, getEvent, updateEvent, deleteEvent
 
 const eventRouter: Router = express.Router();
 
-eventRouter.post("/events", createEvent);
+eventRouter.post("/events", validateRequest(postSchemas.create), createEvent);
+
 // eventRouter.get("/events", getAllEvents);
 // eventRouter.get("/events/:id", getEvent);
 // eventRouter.put("/events/:id", updateEvent);
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 import eventRouter from "./api/v1/routes/eventRoutes";
+import morgan from "morgan";
 
 // Initialize Express application
 const app: Express = express();
@@ -8,6 +9,9 @@ app.use(express.json());
 
 // Route handler for items
 app.use("/api/v1", eventRouter);
+
+// Integrate Morgan for HTTP request logging.
+app.use(morgan("combined"));
 
 app.get("/api/v1/health", (req, res) => {
     res.json({
