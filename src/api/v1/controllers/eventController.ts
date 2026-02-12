e9d996a4925d1df23d171cba77e1b55aa08cee3c
Leann Mekler <lmekler@rrc.ca>
2026-02-12 12:58:42 -0600

    Create a controller function for event creation

---
+++ src/api/v1/controllers/eventController.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from "express";
+import { Event } from "../models/eventModel";
+import { createEventService,} from "../services/eventService";
+import { HTTP_STATUS } from "../../../../src/constants/httpConstants";
+
+export const createEvent = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => 
+{
+    try 
+    {
+        const { name, date, capacity } = req.body;
+        const event: Event = { name, date, capacity };
+
+        const eventResponse: Event = await createEventService(event);
+
+        res.status(HTTP_STATUS.CREATED).json(
+        {
+            message: "Event created",
+            data: eventResponse
+        });
+    }
+    catch (error: unknown) 
+    {
+        next(error);
+    }
+};
