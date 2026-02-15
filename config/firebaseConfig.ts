0c0b6e33ee01932f03fba03eb8ba14c15ac7a2b1
Leann Mekler <lmekler@rrc.ca>
2026-02-14 19:29:47 -0600

    Import provided firestore repository files

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,21 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
+import { getFirestore, Firestore } from "firebase-admin/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyC8sJXnfabeNnOEckN5k6SVc_ASOwvQHqY",
-  authDomain: "bed-assignment-3-6ffc6.firebaseapp.com",
-  projectId: "bed-assignment-3-6ffc6",
-  storageBucket: "bed-assignment-3-6ffc6.firebasestorage.app",
-  messagingSenderId: "485291593542",
-  appId: "1:485291593542:web:b05e5d210caeb593599f47",
-  measurementId: "G-TKYVXLCH1F"
-};
+import * as serviceAccount from "../bed-assignment-3-6ffc6-firebase-adminsdk-fbsvc-9305f20a11.json";
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Initialize the Firebase app with the service account credentials
+// This step is necessary before you can use any Firebase services
+initializeApp({
+    credential: cert(serviceAccount as ServiceAccount),
+});
+
+// Get a reference to the Firestore service
+// This creates a Firestore instance that you can use to interact with your database
+const db: Firestore = getFirestore();
+
+export { db };
